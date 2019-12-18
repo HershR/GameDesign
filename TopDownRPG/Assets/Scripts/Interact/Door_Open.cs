52aa3f4a3a15e642a385f39ecee7a86e36f9e7b2
HershR <45576093+HershR@users.noreply.github.com>
2019-12-17 17:38:01 -0800

    Added Enemy Controller
    Also fixed Scripts folder

---
+++ TopDownRPG/Assets/Scripts/Interact/Door_Open.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door_Open : Interactable
+{
+    Item key;
+    
+
+    public override void Interact(){
+        base.Interact();
+       
+    }
+    void OnCollisionEnter(Collision other){
+        if(other.gameObject.tag=="Player"){
+
+        }
+    }
+}
