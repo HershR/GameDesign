fdffafa2f41bed910c367ab0cd0d2f581943f5f4
Hersh <hersh1999@gmail.com>
2020-01-17 10:31:25 -0800

    Interactable2 Works
    Player can interact with objects when in range

---
+++ TopDownRPG/Assets/Scripts/Test/Interactable2.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable2 : MonoBehaviour
+{
+    public float radius =3f;
+    bool isFocus=false;
+    bool hasInteracted=false;
+    Transform player;
+    private DialogueManager ConverationTrig;
+    public Transform interactionTransform;
+    public Dialogue dialogue;
+    public string InteractKey="f";
+    public virtual void Interact()
+    {
+            //This method is meant to be overwritten
+            Debug.Log("Interact");
+
+    }
+    void Update(){
+        if(isFocus==true&&hasInteracted==false){
+            float distance=Vector3.Distance(player.position,interactionTransform.position);
+            if(distance<=radius){
+                Interact();
+                hasInteracted=true;
+            }
+        }
+    }
+    public void OnFocused(Transform playerTransform){
+        isFocus=true;
+        player=playerTransform;
+        hasInteracted=false;
+    }
+    public void OnDefocused(){
+        FindObjectOfType<DialogueManager>().EndDialogue();
+        isFocus=false;
+        player=null;
+        hasInteracted=false;
+    }
+
+    public void TriggerConversation(){
+        FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+    }
+    public void FaceTarget(){
+        Vector3 direction= (player.position-transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x,0,direction.z));
+        transform.rotation=Quaternion.Slerp(transform.rotation,lookRotation,1.0f);
+    }
+    void OnDrawGizmosSelected(){
+        if(interactionTransform==null)
+            interactionTransform=transform;
+        Gizmos.color=Color.yellow;
+        Gizmos.DrawWireSphere(interactionTransform.position,radius);
+    }
+}
+
--- TopDownRPG/Assets/Scripts/Test/PlayerCont.cs
+++ TopDownRPG/Assets/Scripts/Test/PlayerCont.cs
@@ -7,10 +7,15 @@ public class PlayerCont : MonoBehaviour
     public float speed;
     private CharacterController controller;
     private Vector3 moveDirection;
+    public LayerMask InteractMask;
+    public Transform origin;
+    public Interactable2 focus;
+    public float Range;
     // Start is called before the first frame update
     void Start()
     {
         controller = gameObject.GetComponent<CharacterController>();
+        //Range=2.0f;
 
     }
 
@@ -21,5 +26,37 @@ public class PlayerCont : MonoBehaviour
         moveDirection=moveDirection.normalized*speed;
         moveDirection.y = (moveDirection.y + Physics.gravity.y);
         controller.Move(moveDirection * Time.deltaTime);
+
+        //
+        RaycastHit hit;
+        if(Physics.Raycast(origin.transform.position,transform.TransformDirection(Vector3.forward),out hit,Range,InteractMask)){
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+            //Debug.Log("Did Hit");
+            Interactable2 interactable=hit.collider.GetComponent<Interactable2>();
+                if(interactable!=null){
+                    if(Input.GetKeyDown(interactable.InteractKey)){
+                        SetFocus(interactable);
+                        Debug.Log("Did player press key: "+Input.GetKeyDown(interactable.InteractKey));
+                    }
+                }
+        }else
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * Range, Color.white);
+            //Debug.Log("Did not Hit");
+        } 
+    }
+    void SetFocus(Interactable2 newFocus){
+        if(newFocus!=focus){
+            if(focus!=null)
+                focus.OnDefocused();
+            focus=newFocus;
+        }
+        newFocus.OnFocused(transform);         
+    }
+    void RemoveFoucs(){
+        FindObjectOfType<DialogueManager>().EndDialogue();
+        if(focus!=null)
+        focus.OnDefocused();
+        focus=null;
     }
 }
