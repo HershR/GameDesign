2ed084f6ac6e34b2e55eb5e5723ee4c1ac17ae94
Hersh <hersh1999@gmail.com>
2020-01-17 09:20:25 -0800

    Added Test scene
    New scene with new player controller and camera movement

---
+++ TopDownRPG/Assets/Scripts/Test/CameraCont.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraCont : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset;
+    public bool useOffsetValue;
+    public float rotateSpeed;
+    public Transform piviot;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(!useOffsetValue){
+            offset=target.position-transform.position;
+        }
+        piviot.transform.position=target.transform.position;
+        piviot.transform.parent=target.transform;
+       // Cursor.lockstate=CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Mouse X")*rotateSpeed;
+        target.Rotate(0,horizontal,0);
+
+        float desiredYAngle =target.eulerAngles.y;
+        Quaternion rotation=Quaternion.Euler(0,desiredYAngle,0);
+        transform.position=target.position-(rotation*offset);
+        transform.LookAt(target);
+        
+    }
+}
---
+++ TopDownRPG/Assets/Scripts/Test/PlayerCont.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCont : MonoBehaviour
+{
+    public float speed;
+    private CharacterController controller;
+    private Vector3 moveDirection;
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = gameObject.GetComponent<CharacterController>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveDirection = (transform.forward*Input.GetAxis("Vertical"))+(transform.right*Input.GetAxis("Horizontal"));
+        moveDirection=moveDirection.normalized*speed;
+        moveDirection.y = (moveDirection.y + Physics.gravity.y);
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+}
